ba65c55eea7cad3d1338468c6c5787705d3a8d98
Ael <erwan.vallienne@viacesi.fr>
2017-01-20 10:59:40 +0100

    Update models' interface

--- server/src/app/model/postgres/interfaces/PostgresModel.ts
+++ server/src/app/model/postgres/interfaces/PostgresModel.ts
@@ -2,8 +2,14 @@
  * Created by soundit on 17/01/2017.
  */
 
+import {Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+
 
 interface PostgresModel {
+
+    createTime: Date;
+    updateTime: Date;
+    deleted: boolean;
 }
 
 export = PostgresModel;
