1cec621510f346891f73f9d525cb560d4987da26
Aelith <erwan.vallienne@viacesi.fr>
2017-01-24 14:06:41 +0100

    Added method + comment

--- server/src/app/repository/postgres/PlaylistRepository.ts
+++ server/src/app/repository/postgres/PlaylistRepository.ts
@@ -26,7 +26,15 @@ class PlaylistRepository extends BaseRepository<Playlist> {
     }
 
 
+    /************  ***********/
+    /******* Retrieve ********/
+    /************  ***********/
 
+
+    /**
+     * Retrieve all playlists, fully hydrated
+     * @param callback
+     */
     retrieveHydrated (callback: (error:any, result: any) => any) {
 
         DataAccessPostgres.connect()
@@ -46,7 +54,64 @@ class PlaylistRepository extends BaseRepository<Playlist> {
             });
     }
 
+    /**
+     * Retrieve all playlists, hydrated with chosen fields
+     * @param fields
+     * @param callback
+     */
+    retrieveCustomHydrated (fields: Property[], callback: (error: any, result: any) => any) {
 
+        let QB = DataAccessPostgres.connect()
+            .getRepository(Playlist)
+            .createQueryBuilder(Playlist.getTableName());
+
+
+        if (fields.indexOf(Property.PlaylistType) > -1 ) {
+            QB.innerJoinAndSelect(Playlist.getTableName() + ".playlistType", "playlistType");
+        }
+        if (fields.indexOf(Property.User) > -1 ) {
+            QB.innerJoinAndSelect(Playlist.getTableName() + ".user", "user");
+        }
+
+
+
+        if (fields.indexOf(Property.SongSources) > -1) {
+            QB
+                .innerJoinAndSelect(Playlist.getTableName() + ".playlistSongs", "playlistSongs")
+                .innerJoinAndSelect("playlistSongs.song", "song")
+                .innerJoinAndSelect("song.songSource", "songSource");
+        }
+        else if (fields.indexOf(Property.Songs) > -1)
+        {
+            QB
+                .innerJoinAndSelect(Playlist.getTableName() + ".playlistSongs", "playlistSongs")
+                .innerJoinAndSelect("playlistSongs.song", "song");
+        }
+        else if (fields.indexOf(Property.PlaylistSongs) > -1 ) {
+            QB.innerJoinAndSelect(Playlist.getTableName() + ".playlistSongs", "playlistSongs");
+        }
+
+
+        QB
+            .getMany()
+            .then((result) => {
+                callback(null, result);
+            })
+            .catch(e => {
+                callback(e, null);
+            });
+    }
+
+
+    /***********  ***********/
+    /******** By Id *********/
+    /***********  ***********/
+
+    /**
+     * Retrieve fully hydrated playlists by id
+     * @param idPlaylist
+     * @param callback
+     */
     findHydratedById (idPlaylist: number, callback: (error: any, result: any) => any) {
 
         DataAccessPostgres.connect()
@@ -67,7 +132,68 @@ class PlaylistRepository extends BaseRepository<Playlist> {
             });
     }
 
+    /**
+     * Retrieve hydrated playlists with chosen fields by id
+     * @param idPlaylist
+     * @param fields
+     * @param callback
+     */
+    findCustomHydratedById (idPlaylist: number, fields: Property[], callback: (error: any, result: any) => any) {
 
+        let QB = DataAccessPostgres.connect()
+            .getRepository(Playlist)
+            .createQueryBuilder(Playlist.getTableName());
+
+
+        if (fields.indexOf(Property.PlaylistType) > -1 ) {
+            QB.innerJoinAndSelect(Playlist.getTableName() + ".playlistType", "playlistType");
+        }
+        if (fields.indexOf(Property.User) > -1 ) {
+            QB.innerJoinAndSelect(Playlist.getTableName() + ".user", "user");
+        }
+
+
+
+        if (fields.indexOf(Property.SongSources) > -1) {
+            QB
+                .innerJoinAndSelect(Playlist.getTableName() + ".playlistSongs", "playlistSongs")
+                .innerJoinAndSelect("playlistSongs.song", "song")
+                .innerJoinAndSelect("song.songSource", "songSource");
+        }
+        else if (fields.indexOf(Property.Songs) > -1)
+        {
+            QB
+                .innerJoinAndSelect(Playlist.getTableName() + ".playlistSongs", "playlistSongs")
+                .innerJoinAndSelect("playlistSongs.song", "song");
+        }
+        else if (fields.indexOf(Property.PlaylistSongs) > -1 ) {
+            QB.innerJoinAndSelect(Playlist.getTableName() + ".playlistSongs", "playlistSongs");
+        }
+
+
+        QB
+            .where("\"" + Playlist.getTableName() + "\".id = :idP", {idP: idPlaylist})
+            .getMany()
+            .then((result) => {
+                callback(null, result);
+            })
+            .catch(e => {
+                callback(e, null);
+            });
+    }
+
+
+
+
+    /***********  ***********/
+    /****** By User Id ******/
+    /***********  ***********/
+
+    /**
+     * Retrieve playlists by user id
+     * @param idUser
+     * @param callback
+     */
     findByUserId (idUser: number, callback: (error: any, result: any) => any) {
 
         DataAccessPostgres.connect()
@@ -83,7 +209,11 @@ class PlaylistRepository extends BaseRepository<Playlist> {
             });
     }
 
-
+    /**
+     * Retrieve fully hydrated playlists by user id
+     * @param idUser
+     * @param callback
+     */
     findHydratedByUserId (idUser: number, callback: (error: any, result: any) => any) {
 
         DataAccessPostgres.connect()
@@ -105,6 +235,12 @@ class PlaylistRepository extends BaseRepository<Playlist> {
     }
 
 
+    /**
+     * Retrieve hydrated playlists with chosen fields by user id
+     * @param idUser
+     * @param fields
+     * @param callback
+     */
     findCustomHydratedByUserId (idUser: number, fields: Property[], callback: (error: any, result: any) => any) {
 
         let QB = DataAccessPostgres.connect()
