908c456c618a471098ca69472830f14d69333893
Unknown <lakio2121@gmail.com>
2017-01-29 16:13:55 +0100

    playlistType get add

--- server/src/app/business/PlaylistBusiness.ts
+++ server/src/app/business/PlaylistBusiness.ts
@@ -6,11 +6,13 @@ import PlaylistRepository = require("../repository/postgres/PlaylistRepository")
 import {Playlist} from "../model/postgres/Playlist";
 import BaseBusiness = require("./interfaces/BaseBusiness");
 import PlaylistSongRepository = require("../repository/postgres/PlaylistSongRepository");
+import PlaylistTypeRepository = require("../repository/postgres/PlaylistTypeRepository");
 
 class PlaylistBusiness extends BaseBusiness<Playlist> {
 
     private playlistRepository: PlaylistRepository;
     private playlistSongRepository: PlaylistSongRepository;
+    private playlistTypeRepository: PlaylistTypeRepository;
 
     constructor () {
         super(Playlist);
@@ -40,6 +42,10 @@ class PlaylistBusiness extends BaseBusiness<Playlist> {
 
     findByUserId(id: number, callback: (error: any, result: any) => void){
         this.playlistRepository.findByUserId(id,callback);
+    }
+
+    getPlaylistTypeById(id: number, callback : (error : any, result:any) => void) {
+        this.playlistTypeRepository.findById(id,callback);
     }
 
 }
